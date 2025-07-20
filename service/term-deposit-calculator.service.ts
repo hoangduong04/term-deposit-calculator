44b0f1c45ea52544de54e4d4ff9c898e29d9454c
Dan Nguyen <d.hnguyen@outlook.com>
2025-07-20 15:38:47 +1000

    feat: add function to calculate compounded deposit amount

--- service/term-deposit-calculator.service.ts
+++ service/term-deposit-calculator.service.ts
@@ -11,4 +11,30 @@ export class TermDepositCalculatorService {
   ): number {
     return -1;
   }
+
+  /**
+   * Recursive functions to calculate compounded amount based on number of interest paid
+   * @param amount
+   * @param interestRate
+   * @param totalCount
+   * @param currentCount
+   * @returns
+   */
+  private compound(
+    amount: number,
+    interestRate: number,
+    totalCount: number,
+    currentCount: number
+  ): number {
+    if (totalCount === currentCount) return amount;
+
+    const compoundedAmount = amount * (1 + interestRate / 100);
+
+    return this.compound(
+      compoundedAmount,
+      interestRate,
+      totalCount,
+      currentCount + 1
+    );
+  }
 }
