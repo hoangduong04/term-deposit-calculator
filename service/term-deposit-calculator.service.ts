0419bf261fa98c948f420439a46579c7c5bfb8ef
Dan Nguyen <d.hnguyen@outlook.com>
2025-07-20 15:05:05 +1000

    chore: scaffolding term deposit class and interfaces

---
+++ service/term-deposit-calculator.service.ts
@@ -0,0 +1,12 @@
+export class TermDepositCalculatorService {
+  constructor() {}
+
+  public calculateInterest(
+    startingAmount: number,
+    interestRate: number,
+    investmentTerm: number,
+    interestPaid: any
+  ): number {
+    return -1;
+  }
+}
